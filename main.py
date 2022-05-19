3c5abb1bccbf9125fc85ddd0be01e8247621501b
Colin Braun <electrolitic21@gmail.com>
2022-05-19 13:48:02 -0400

    Shift variables around slightly

--- main.py
+++ main.py
@@ -58,9 +58,28 @@ for tet in tetrahedrons:
                 continue
             edge2 = all_edges[edgej]
             node_ik, node_jk = all_nodes[edge1.node1], all_nodes[edge1.node2]
+
+            # Find the indices of the edge of interest
+            indices_k = [np.argwhere(tet.nodes == edge2.node1)[0][0], np.argwhere(tet.nodes == edge2.node2)[0][0]]
+            # The simplex coordinates for nodes i and j of edge l
+            a_ik, a_jk = all_cofactors[indices_k]
+            # Necessary constants from NASA paper eqs. 163-172
+            Axk = a_ik[0] * a_jk[1] - a_ik[1] * a_jk[0]
+            Bxk = a_ik[2] * a_jk[1] - a_ik[1] * a_jk[2]
+            Cxk = a_ik[3] * a_jk[2] - a_ik[2] * a_jk[3]
+            Ayk = a_ik[0] * a_jk[2] - a_ik[2] * a_jk[0]
+            Byk = a_ik[1] * a_jk[2] - a_ik[2] * a_jk[1]
+            Cyk = a_ik[3] * a_jk[2] - a_ik[2] * a_jk[3]
+            Azk = a_ik[0] * a_jk[3] - a_ik[3] * a_jk[0]
+            Bzk = a_ik[1] * a_jk[3] - a_ik[3] * a_jk[1]
+            Czk = a_ik[2] * a_jk[3] - a_ik[3] * a_jk[2]
             # If working with input port edges, need to do different integral
             if edgei in boundary_input_edge_numbers and edgej in boundary_input_edge_numbers:
                 # TODO: Implement this integral and logic
+                # Perform the (n_hat x N_i) \dot (n_hat x N_j) integral
+                # For n_hat = z_hat, this turns out to be the same as N_i \dot N_j, so integral is same as inhomo wg
+                # Need all 3 nodes first
+                # Necessary constants from NASA paper eqs. 68-72.
                 pass
             # If working with output port edges, need to do different integral
             elif edgei in boundary_output_edge_numbers and edgej in boundary_output_edge_numbers:
@@ -68,27 +87,11 @@ for tet in tetrahedrons:
                 pass
             # Otherwise we are working with an inner edge (not on boundary). Do necessary integral.
             else:
-                # Find the indices of the edge of interest
-                indices_k = [np.argwhere(tet.nodes == edge2.node1)[0][0], np.argwhere(tet.nodes == edge2.node2)[0][0]]
-                # The simplex coordinates for nodes i and j of edge l
-                a_ik, a_jk = all_cofactors[indices_k]
-                # Necessary constants from NASA paper eqs. 163-172
-                Axk = a_ik[0] * a_jk[1] - a_ik[1] * a_jk[0]
-                Bxk = a_ik[2] * a_jk[1] - a_ik[1] * a_jk[2]
-                Cxk = a_ik[3] * a_jk[2] - a_ik[2] * a_jk[3]
-                Ayk = a_ik[0] * a_jk[2] - a_ik[2] * a_jk[0]
-                Byk = a_ik[1] * a_jk[2] - a_ik[2] * a_jk[1]
-                Cyk = a_ik[3] * a_jk[2] - a_ik[2] * a_jk[3]
-                Azk = a_ik[0] * a_jk[3] - a_ik[3] * a_jk[0]
-                Bzk = a_ik[1] * a_jk[3] - a_ik[3] * a_jk[1]
-                Czk = a_ik[2] * a_jk[3] - a_ik[3] * a_jk[2]
-
                 # Compute the curl(N_i) \dot curl(N_j) part of K_ij
                 curl_dot_curl_part = edge1.length*edge2.length / 324 / tet.volume**3 * (Czl*Czk + Cxl*Cxk + Byl*Byk)
 
-                # Build constants for N_i \dot N_j part of K_ij
+                # Build constants for N_i \dot N_j integral (part of K_ij and b_i)
                 # These constants come from NASA paper eq. 182
-                # TODO: Compute x_mean, y_mean, z_mean for the tetrahedron
                 I1 = Axl*Axk + Ayl*Ayk + Azl*Azk
                 I2 = (Ayl*Byk + Ayk*Byl + Azl*Bzk + Azk*Bzl) * x_mean
                 I3 = (Axl*Bxk + Axk*Bxl + Azl*Czk + Azk*Czl) * y_mean
